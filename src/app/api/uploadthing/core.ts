4bd7d19045037f07abbcf29a8b80ead3f32fed64
AhmedGaber77 <ahmed255177@gmail.com>
2024-03-13 12:45:40 +0200

    Add chat Wrapper

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,76 @@
+import { db } from '@/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { UploadThingError } from 'uploadthing/server';
+// import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { getPineconeClient } from '@/lib/pinecone';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
+    .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+
+      if (!user || !user.id) throw new Error('UNAUTHORIZED');
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          // url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          url: file.url,
+          uploadStatus: 'PROCESSING',
+        },
+      });
+
+      try {
+        const response = await fetch(file.url);
+        const blob = await response.blob();
+
+        // const loader = new PDFLoader(blob);
+        // const pageLevelDocs = await loader.load();
+        // const pagesAmt = pageLevelDocs.length;
+        // const pinecone = getPineconeClient();
+        // const pineconeIndex = (await pinecone).Index('pdfs');
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        // await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+        //   pineconeIndex,
+        //   namespace: createdFile.id,
+        // });
+
+        await db.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        console.error(error);
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
